864624957aa95e83403ead6edbb4cbc718232239
Roxanaa1 <roxanaenache2003@gmail.com>
2024-05-21 02:48:48 +0300

    Update

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,39 @@
+package com.example.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+enum UserRole{
+    ROLE_USER, ROLE_EDITOR
+}
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="users")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_utilizator")
+    private Long id;
+
+    @Column(name = "nume")
+    private String name;
+
+    @Column(name = "utilizator", unique = true)
+    private String username;
+
+    @Column(name = "parola")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol")
+    private UserRole role;
+
+
+}
+
+

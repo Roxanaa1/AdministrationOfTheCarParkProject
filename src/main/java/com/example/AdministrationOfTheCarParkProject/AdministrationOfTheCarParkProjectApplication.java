98dd03abf421de82f23d34893021cbf40a7eaa63
Roxanaa1 <roxanaenache2003@gmail.com>
2024-05-21 02:33:16 +0300

    First commit

---
+++ src/main/java/com/example/AdministrationOfTheCarParkProject/AdministrationOfTheCarParkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.AdministrationOfTheCarParkProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrationOfTheCarParkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrationOfTheCarParkProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AdministrationOfTheCarParkProject/AdministrationOfTheCarParkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdministrationOfTheCarParkProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationOfTheCarParkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
